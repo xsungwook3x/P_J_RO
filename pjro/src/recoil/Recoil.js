14c2d81e07a07ca5179a2859d56c29782ba1876c
Seonguk <xsungwook3x@naver.com>
2022-10-29 22:37:53 +0900

    fix recoil

--- pjro/src/recoil/Recoil.js
+++ pjro/src/recoil/Recoil.js
@@ -2,45 +2,12 @@ import {atom,selector} from 'recoil';
 
 export const todoListState=atom({
     key:'todoListState',
-    default:[
-        {
-        id: 1,
-        title: '프로젝트 생성하기',
-        done: true
-    },
-    {
-        id: 2,
-        title: '컴포넌트 스타일링하기',
-        done: true
-    },
-    {
-        id: 3,
-        title: 'Context 만들기',
-        done: false
-    },
-    {
-        id: 4,
-        title: '기능 구현하기',
-        done: false
-    }]
+    default:[]
 })
 
 export const routineListState=atom({
     key:'routineListState',
-    default:[
-        {
-            id: 1,
-            title: 'CS 스터디',
-            mon: true,
-            tue: true,
-            wed: false,
-            thr: false,
-            fri: false,
-            sat: false,
-            sun: false
-
-        }
-    ]
+    default:[]
 });
 
 export const countTodoWorkState = selector({
